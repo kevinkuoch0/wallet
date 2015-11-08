dfb041e1c31aecd3a23dc7053941a4d8d8d45d66
Kevin Kuoch <kevinkuoch0@gmail.com>
2015-11-07 22:06:16 -0500

    created debit table

---
+++ app/models/debit.rb
@@ -0,0 +1,6 @@
+class Debit < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151108030136) do
+ActiveRecord::Schema.define(version: 20151108030541) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -31,6 +31,30 @@ ActiveRecord::Schema.define(version: 20151108030136) do
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+
+  create_table "debits", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.integer  "user_id"
+    t.string   "cardnumber"
+    t.string   "cardtype"
+    t.integer  "expirationmonth"
+    t.integer  "expirationyear"
+    t.decimal  "balance"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+  end
+
+  add_index "debits", ["email"], name: "index_debits_on_email", unique: true
+  add_index "debits", ["reset_password_token"], name: "index_debits_on_reset_password_token", unique: true
 
   create_table "users", force: :cascade do |t|
     t.string   "email"
